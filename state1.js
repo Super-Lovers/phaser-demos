a4cfc720a2d6c8037b6c981549a05121ab7ca1ee
145-Poodles <niki.ivanov98@gmail.com>
2017-11-24 11:22:08 +0100

    Completed second state using tilemaps

--- state1.js
+++ state1.js
@@ -1,10 +1,53 @@
 demo.state1 = function() {};
+var cursors, catcher, vel = 500, rocks, grass;
 demo.state1.prototype = {
-    preload: function(){},
-    create: function(){
-        game.stage.backgroundColor = '#dddddd';
-        console.log('You are in state one');
-
+    preload: function(){
+        game.load.tilemap('field', 'img/state1/field.json', null, Phaser.Tilemap.TILED_JSON);
+        game.load.image('grassTiles', 'img/state1/grassTiles.png');
+        game.load.image('rockTiles', 'img/state1/rockTiles.png');
+        game.load.image('catcher', 'img/catcherBig.png');
     },
-    update: function(){}
+    create: function(){
+        game.physics.startSystem(Phaser.Physics.ARCADE);
+        game.stage.backgroundColor = '#DDDDDD';
+
+        var map = game.add.tilemap('field');
+        map.addTilesetImage('grassTiles');
+        map.addTilesetImage('rockTiles');
+
+        grass = map.createLayer('grass');
+        rocks = map.createLayer('rocks');
+
+        map.setCollisionBetween(1, 9, true, 'rocks');
+        map.setCollision(11, true, 'grass');
+
+
+        catcher = game.add.sprite(300, 200, 'catcher');
+        game.physics.enable(catcher);
+        catcher.anchor.setTo(0.5, 0.5);
+        catcher.scale.setTo(0.4, 0.4);
+
+        cursors = game.input.keyboard.createCursorKeys();
+    },
+    update: function(){
+        game.physics.arcade.collide(catcher, grass);
+        game.physics.arcade.collide(catcher, rocks);
+
+        if (cursors.up.isDown) {
+            catcher.body.velocity.y = -vel;
+        } else if (cursors.down.isDown) {
+            catcher.body.velocity.y = vel;
+        }
+        else {
+            catcher.body.velocity.y = 0;
+        }
+        if (cursors.left.isDown) {
+            catcher.body.velocity.x = -vel;
+        } else if (cursors.right.isDown) {
+            catcher.body.velocity.x = vel;
+        }
+        else {
+            catcher.body.velocity.x = 0;
+        }
+    }
 };
